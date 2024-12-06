1ed40e932fed4cac75666e4c9bf3b2467ddeb071
Allan Giovanni Matias <alangiovanepaes@gmail.com>
2024-12-05 21:09:34 -0300

    rest/spread operator

--- reviewJS/script.js
+++ reviewJS/script.js
@@ -160,9 +160,34 @@ title; // The Cyberiad
 genres // an array of genres
 
 // indexes of the genres array
-//      0             1
-const [primaryGenre, secondaryGenre] = genres
+
+// ...array = takes the value of an array
+//      0             1              rest of items
+const [primaryGenre, secondaryGenre, ...otherGenres] = genres
 primaryGenre // science fiction
 secondaryGenre // humor
+otherGenres // a list with other genres
+
+// i trying to add an genre in array
+// basecally, this mode changes the principal array, while the teacher method,
+// create a new array with the new value
+
+// const newGenres = genres.push('epic')
+// genres += epic
 
 
+// what teacher does
+const newGenres = [...genres, 'epic']
+newGenres 
+
+const updatedBook = {
+    ...book,
+    
+    // adding a new property
+    moviePublicationDate: '2000-01-20',
+
+    // overwriting an existing property
+    pages: 23
+    }
+updatedBook;
+log(updatedBook)
