516b04ff06bc5bcc5cc708c030ab60e9a8737572
Allan Giovanni Matias <alangiovanepaes@gmail.com>
2025-01-12 21:56:45 -0300

    react fragments

--- pizza-menu/src/index.js
+++ pizza-menu/src/index.js
@@ -81,10 +81,17 @@ function Menu() {
 
         <main className='menu'>
             <h2>Our Menu</h2>
+
+
             {pizzasArrayLength > 0 ? (
-                <ul className='pizzas'>
-                    {pizzas.map((pizzaObje) => <Pizza obj={pizzaObje}></Pizza>)}
-                </ul>
+                <>
+                    <p>Authentic Italian cuisine. 6 creative dishes to choose from.
+                        All from our stone oven, all organic, all delicious</p>
+                    <ul className='pizzas'>
+                        {pizzas.map((pizzaObje) => <Pizza obj={pizzaObje}></Pizza>)}
+                    </ul>
+                </>
+
             ) : (
                 <div>We're working on our menu. Please come back later.</div>
             )
