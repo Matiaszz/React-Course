569adb1b34ad5f3e4cbb3a297ac38a828bebdd07
Allan Giovanni Matias <alangiovanepaes@gmail.com>
2025-01-14 12:01:11 -0300

    use state in react

--- steps/src/App.js
+++ steps/src/App.js
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 const messages = [
   "Learn React ⚛️",
   "Apply for jobs 💼",
@@ -6,24 +8,18 @@ const messages = [
 
 export default function App() {
 
-  const step = 1
+  const [step, setStep] = useState(1);
+  const handlePrevious = () => setStep((page) => page - 1 > 0 ? page - 1 : page);
+  const handleNext = () => setStep((page) => page + 1 > messages.length ? page : page + 1);
 
-
-  const handlePrevious = () => {
-    alert('previous');
-  }
-
-  const handleNext = () => {
-    alert('next');
-  }
 
 
   return (
     <div className="steps">
       <div className="numbers">
-        <div className={`${step >= 1 ? 'active' : ''}`}>1</div>
-        <div className={`${step >= 2 ? 'active' : ''}`}>2</div>
-        <div className={`${step >= 3 ? 'active' : ''}`}>3</div>
+        <div className={step >= 1 ? 'active' : ''}>1</div>
+        <div className={step >= 2 ? 'active' : ''}>2</div>
+        <div className={step >= 3 ? 'active' : ''}>3</div>
       </div>
 
       <p className="message">
@@ -32,7 +28,7 @@ export default function App() {
 
       <div className="buttons">
         <button style={{ backgroundColor: '#7950f2', color: '#fff' }}
-          onClick={() => handlePrevious}
+          onClick={handlePrevious}
 
 
 
